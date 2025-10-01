678ff3578e2114cd87170f35fd053cd2b55d363c
jiesou <84175239+jiesou@users.noreply.github.com>
2025-10-01 21:09:16 +0800

    ci: 使用无 binary 的 prisma 以便于在 termux 开发。同时优化 docker compose 和 .env

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,10 @@
+import { PrismaPg } from 'npm:@prisma/adapter-pg'
 import { PrismaClient } from "./prisma/generated/client.ts";
 
+const connectionString = `${Deno.env.get("DATABASE_URL")}`
+const adapter = new PrismaPg({ connectionString });
 // 单例 prisma
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ adapter });
 prisma.$connect().then(() => {
   console.log("Prisma 数据库连接已建立");
   console.log(`DATABASE_URL: ${Deno.env.get("DATABASE_URL")}`);
